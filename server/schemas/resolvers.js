62e77f821fedcb084d7a869093229e0760f9d9da
jeannav <120337476+jeannav@users.noreply.github.com>
2023-05-24 08:16:00 -0700

    set up me query in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,46 +3,34 @@ const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-  Query: {
-    me: async () => {
-      return User.find({});
-    },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
-    },
-  },
-  Mutation:  {
-  createUser: async (parents, args) => {
-    const user = await User.create(args);
-    const token = signToken(user);
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const user = await User.findOne({ _id: context.user._id })
 
-    return {token, user}
-  },
-
-    login: async (parent, {email, password}) => {
-
+                return user
+            }
+            throw new AuthenticationError("User Not Logged In!")
+        }
     },
-    saveBook: async (parent, {bookData}, context) => {
+    Mutation: {
+        createUser: async (parents, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
+            return { token, user }
+        },
+
+        login: async (parent, { email, password }) => {
+
+        },
+        saveBook: async (parent, { bookData }, context) => {
+
+        },
+        deleteBook: async (parent, { bookId }, context) => {
+
+        }
     },
-    deleteBook: async (parent, {bookId}, context) => {
-        
-    }
-   
-   // createMatchup: async (parent, args) => {
-   //   const matchup = await Matchup.create(args);
-   //   return matchup;
-   // },
-   // createVote: async (parent, { _id, techNum }) => {
-   //   const vote = await Matchup.findOneAndUpdate(
-   //     { _id },
-   //     { $inc: { [`tech${techNum}_votes`]: 1 } },
-   //     { new: true }
-   //   );
-   //   return vote;
-   // },
-  },
 };
 
 module.exports = resolvers;
