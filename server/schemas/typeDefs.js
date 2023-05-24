ae6db2d9029b6b54b94425143d455777ecab1e0f
jeannav <120337476+jeannav@users.noreply.github.com>
2023-05-24 08:25:09 -0700

    filled in typedefs for createUser and login mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -24,8 +24,8 @@ const typeDefs = gql`
     }
 
    type Mutation {
-    createUser()
-    login()
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!) : Auth
     saveBook()
     deleteBook()
    }
