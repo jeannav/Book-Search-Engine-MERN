cd4bfd0d8214abb1e177526470db4cbcd4373695
jeannav <120337476+jeannav@users.noreply.github.com>
2023-05-24 08:23:23 -0700

    set up login mutation in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -20,9 +20,22 @@ const resolvers = {
 
             return { token, user }
         },
-
         login: async (parent, { email, password }) => {
-
+            // find the user thats trying to login by email
+            // if that email isnt linked to a user throw AuthenticationError
+            const user = await User.findOne({ email })
+            if (!user) {
+                throw new AuthenticationError("Credentials dont match!")
+            }
+            // after finding the email match, check if password entered, matches password for that email
+            // if password is wrong, tell them user credential dont match
+            const passwordValidity = await user.isCorrectPassword(password)
+            if (!passwordValidity) {
+                throw new AuthenticationError("Credentials dont match!")
+            }
+            // create token and return user/token
+            const token = signToken(user)
+            return { token, user }
         },
         saveBook: async (parent, { bookData }, context) => {
 
