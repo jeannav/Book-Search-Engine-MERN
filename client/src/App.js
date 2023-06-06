5db3ce7a3da67c06196b98b3470e64cd97d10290
jeannav <120337476+jeannav@users.noreply.github.com>
2023-06-05 18:13:52 -0700

    added code for authlink to APP.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,18 @@ const httpLink = createHttpLink({
   uri: "/graphql"
 })
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
