76f75c04793a9c1090b913b9d2b31358c9ecc952
jeannav <120337476+jeannav@users.noreply.github.com>
2023-06-12 10:09:07 -0700

    new mongodburi

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://googlebooksapi:pasword12345@cluster0.biksjiy.mongodb.net/');
 
 module.exports = mongoose.connection;
